1df8d482dd7f7d61cac61ee80154e45986d35000
Omegamark <grant.mark@gmail.com>
2016-07-19 14:50:17 -0600

    Array to an object of arrays

---
+++ Gal Chal.js
@@ -0,0 +1,13 @@
+
+function toObject {
+  var ccArray = ['4916-2600-1804-0530', '4779-252888-3972', '4252-278893-7978', '4556-4242-9283-2260'];
+  var ccArrayObject = {}
+  for (ccArray) {i = 0; i < ccArray.length; i++}
+       ccArray[i] = ccArrayObject[i]
+      return ccArrayObject
+       }
+var ccArrayString = ccArray.toString();
+
+var ccStringSplit = ccArrayString.split('')
+
+console.log(ccStringSplit)
